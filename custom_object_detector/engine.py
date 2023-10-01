349bf31e5d82ed7a94130ff1e7cf4be2e5a775e0
kvnptl <kevinpatel4400@gmail.com>
2023-10-01 20:24:10 +0200

    [CustomDetector] Integrated torchmetrics class and bbox accuracies

--- custom_object_detector/engine.py
+++ custom_object_detector/engine.py
@@ -5,6 +5,7 @@ from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 
 def train_step(model: torch.nn.Module, 
@@ -41,7 +42,11 @@ def train_step(model: torch.nn.Module,
 
   # Weightagts for each loss function
   W1, W2 = 1.0, 1.0
-
+  
+  # Torchmetrics accuracy
+  metric_cls = torchmetrics.Accuracy(task="multiclass", num_classes=10).to(device)
+  metric_bbox = torchmetrics.detection.MeanAveragePrecision().to(device) # iou_thresholds are the stepped range [0.5,...,0.95] with step 0.05. Else provide a list of floats.
+  
   # Loop through data loader data batches
   for batch, (X, y_cls, y_bbox) in enumerate(dataloader):
       # Send data to target device
@@ -69,14 +74,37 @@ def train_step(model: torch.nn.Module,
 
       # Calculate and accumulate accuracy metric across all batches
       y_pred_class = torch.argmax(torch.softmax(y_pred[0], dim=1), dim=1)
-      train_cls_acc += (y_pred_class == y_cls).sum().item()/len(y_pred[0])
+      train_cls_acc += (y_pred_class == y_cls).sum().item()/len(y_pred[0]) # OPTION 1
+
+      # Using torchmetric
+      metric_cls.update(y_pred_class, y_cls) # OPTION 2
+
+      # Prepare bbox predictions for torchmetric
+      preds = [{'boxes': y_pred[1], 'scores': torch.tensor([1.0]*len(y_pred[1])), 'labels': y_pred_class}]
+      targets = [{'boxes': y_bbox, 'labels': y_cls}]
+      metric_bbox.update(preds, targets)
 
   # Adjust metrics to get average loss and accuracy per batch
   train_cls_loss = train_cls_loss / len(dataloader)
+  train_cls_acc = train_cls_acc / len(dataloader) # OPTION 1
+
+  # Cls acc using torchmetrics
+  train_cls_acc_torchmetrics = metric_cls.compute() # OPTION 2
+  metric_cls.reset()
+  
   train_bbox_loss = train_bbox_loss / len(dataloader) 
+  
+  # Bbox acc using torchmetric
+  train_bbox_acc_torchmetrics = metric_bbox.compute()
+  metric_bbox.reset()
+
+  # Total loss
   train_loss = train_loss / len(dataloader)
-  train_cls_acc = train_cls_acc / len(dataloader)
-  return train_loss, train_cls_acc, train_cls_loss, train_bbox_loss
+
+  # Total accuracy
+  train_acc = ((train_cls_acc_torchmetrics + train_bbox_acc_torchmetrics["map"]) / 2).item() # item() works when a tensor contains a single value, otherwise use .cpu().numpy()
+
+  return train_loss, train_acc, train_cls_loss, train_cls_acc_torchmetrics, train_bbox_loss, train_bbox_acc_torchmetrics
 
 def test_step(model: torch.nn.Module, 
               dataloader: torch.utils.data.DataLoader, 
@@ -109,6 +137,10 @@ def test_step(model: torch.nn.Module,
 
   # Weightagts for each loss function
   W1, W2 = 1.0, 1.0
+
+  # Torchmetrics accuracy
+  metric_cls = torchmetrics.Accuracy(task="multiclass", num_classes=10).to(device)
+  metric_bbox = torchmetrics.detection.MeanAveragePrecision().to(device) # iou_thresholds are the stepped range [0.5,...,0.95] with step 0.05. Else provide a list of floats.
   
   # Turn on inference context manager
   with torch.inference_mode():
@@ -118,11 +150,11 @@ def test_step(model: torch.nn.Module,
           X, y_cls, y_bbox = X.to(device), y_cls.to(device), y_bbox.to(device)
   
           # 1. Forward pass
-          test_pred_logits = model(X)
+          y_pred_test = model(X)
 
           # 2. Calculate and accumulate loss
-          classLoss = loss_fn[0](test_pred_logits[0], y_cls)
-          bboxLoss = loss_fn[1](test_pred_logits[1], y_bbox)
+          classLoss = loss_fn[0](y_pred_test[0], y_cls)
+          bboxLoss = loss_fn[1](y_pred_test[1], y_bbox)
           total_loss = W1 * classLoss + W2 * bboxLoss
           
           test_cls_loss += classLoss.item()
@@ -130,15 +162,35 @@ def test_step(model: torch.nn.Module,
           test_loss += total_loss.item()
           
           # Calculate and accumulate accuracy
-          y_pred_class = torch.argmax(torch.softmax(test_pred_logits[0], dim=1), dim=1)
-          test_cls_acc += (y_pred_class == y_cls).sum().item()/len(test_pred_logits[0])
+          y_pred_class = torch.argmax(torch.softmax(y_pred_test[0], dim=1), dim=1)
+          test_cls_acc += (y_pred_class == y_cls).sum().item()/len(y_pred_test[0])
+
+          # Using torchmetric
+          metric_cls.update(y_pred_class, y_cls) # OPTION 2
+
+          # Prepare bbox predictions for torchmetric
+          preds = [{'boxes': y_pred_test[1], 'scores': torch.tensor([1.0]*len(y_pred_test[1])), 'labels': y_pred_class}]
+          targets = [{'boxes': y_bbox, 'labels': y_cls}]
+          metric_bbox.update(preds, targets)
           
   # Adjust metrics to get average loss and accuracy per batch 
-  test_cls_loss = test_cls_loss / len(dataloader)
-  test_bbox_loss = test_bbox_loss / len(dataloader)
-  test_loss = test_loss / len(dataloader)
   test_cls_acc = test_cls_acc / len(dataloader)
-  return test_loss, test_cls_acc, test_cls_loss, test_bbox_loss
+  test_cls_loss = test_cls_loss / len(dataloader)
+
+  # Using torchmetric
+  test_cls_acc_torchmetrics = metric_cls.compute() # OPTION 2
+  metric_cls.reset()
+
+  test_bbox_acc_torchmetrics = metric_bbox.compute()
+  test_bbox_loss = test_bbox_loss / len(dataloader)
+
+  # Total loss
+  test_loss = test_loss / len(dataloader)
+
+  # Total accuracy
+  test_acc = ((test_cls_acc_torchmetrics + test_bbox_acc_torchmetrics["map"]) / 2).item() # item() works when a tensor contains a single value, otherwise use .cpu().numpy()
+
+  return test_loss, test_acc, test_cls_loss, test_cls_acc_torchmetrics, test_bbox_loss, test_bbox_acc_torchmetrics
 
 def train(model: torch.nn.Module, 
           train_dataloader: torch.utils.data.DataLoader, 
@@ -192,12 +244,12 @@ def train(model: torch.nn.Module,
   
   # Loop through training and testing steps for a number of epochs
   for epoch in tqdm(range(epochs)):
-      train_loss, train_acc, train_cls_loss, train_bbox_loss = train_step(model=model,
+      train_loss, train_acc, train_cls_loss, train_cls_acc, train_bbox_loss, train_bbox_acc = train_step(model=model,
                                           dataloader=train_dataloader,
                                           loss_fn=loss_fn,
                                           optimizer=optimizer,
                                           device=device)
-      test_loss, test_acc, test_cls_loss, test_bbox_loss = test_step(model=model,
+      test_loss, test_acc, test_cls_loss, test_cls_acc, test_bbox_loss, test_bbox_acc  = test_step(model=model,
           dataloader=test_dataloader,
           loss_fn=loss_fn,
           device=device)
