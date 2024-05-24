a5d23bb38a35322244653f8eec6c443ca9280099
kvnptl <kevinpatel4400@gmail.com>
2024-05-25 00:20:14 +0200

    [seg][unet] update test model script

--- segmentation/unet_camvid/test_model.py
+++ segmentation/unet_camvid/test_model.py
@@ -19,7 +19,8 @@ IMAGE_HEIGHT = config.IMAGE_HEIGHT
 IMAGE_WIDTH = config.IMAGE_WIDTH
 PIN_MEMORY = config.PIN_MEMORY
 DATASET_DIR = config.DATASET_DIR
-LOAD_MODEL_FILE = "/home/kpatel2s/work/visual_perception/segmentation/unet_camvid/results/CamVid/2024-05-18_00-09/model/best_model_10.pth"
+LOAD_MODEL_FILE = "/home/kpatel2s/work/visual_perception/segmentation/unet_camvid/results/CamVid/2024-05-24_00-51/model/best_model_96.pth"
+SAVE_PREDICTIONS = False
 
 # Dataset prep
 # Get the class label csv file
@@ -28,6 +29,9 @@ classes = pd.read_csv(DATASET_DIR + '/class_dict.csv', index_col=0)
 # There are total 32 classes
 n_classes = len(classes)
 print(f'[INFO] Total number of classes: {n_classes}')
+
+# Set seed
+utils.set_seed(seed=config.SEED)
 
 model = model.UNet(in_channels=3, out_channels=n_classes).to(DEVICE)
 
@@ -57,9 +61,6 @@ test_dataloader = utils.get_loaders(
     num_workers=NUM_WORKERS
 )
 
-
-loss_fn = nn.CrossEntropyLoss()  # for multi class, use cross entropy
-
 # Output dir
 # Get the folder name from LOAD_MODEL_FILE
 target_dir = "/".join(LOAD_MODEL_FILE.split("/")[:-2])
@@ -77,30 +78,27 @@ with torch.inference_mode():
 
         with torch.no_grad():
             outputs = model(data)
-            loss = loss_fn(outputs, target)
 
             preds = torch.argmax(outputs, dim=1)
             x_cpu = data.cpu()
             preds_cpu = preds.cpu()
             y_cpu = target.cpu()
-            utils.visualize(idx, x_cpu[0].permute(1, 2, 0),
-                            preds_cpu[0], y_cpu[0], folder=target_dir)
+            if SAVE_PREDICTIONS:
+                utils.visualize(idx, x_cpu[0].permute(1, 2, 0),
+                                preds_cpu[0], y_cpu[0], folder=target_dir)
 
-        test_loss += loss.item()
         test_acc += utils.pixel_accuracy(outputs, target)
         test_dice_score += utils.dice_score_fn(outputs, target)
         test_iou_score += utils.iou_score_fn(outputs, target)
 
         tqdm_loop.set_postfix(
-            loss=test_loss / (idx + 1),
             acc=test_acc / (idx + 1),
             dice_score=test_dice_score / (idx + 1),
             iou_score=test_iou_score / (idx + 1)
         )
 
-test_loss = test_loss / len(test_dataloader)
 test_acc = test_acc / len(test_dataloader)
 test_dice_score = test_dice_score / len(test_dataloader)
 test_iou_score = test_iou_score / len(test_dataloader)
 
-print(f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}, Test Dice Score: {test_dice_score:.4f}, Test IoU Score: {test_iou_score:.4f}")
+print(f"Test Acc: {test_acc*100:.2f}, Test Dice Score: {test_dice_score*100:.2f}, Test IoU Score: {test_iou_score*100:.2f}")
